c736a099aeb8c809ecc0b4ed89e04f712538e4ad
Josiah Reeves <82180221+joreeves@users.noreply.github.com>
2022-06-20 09:49:49 -0700

    setup added

---
+++ lazyloader/__init__.py
@@ -0,0 +1,1 @@
+from lazyloader import LazyLoader
---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+https://python-packaging.readthedocs.io/en/latest/minimal.html
+'''
+
+from setuptools import setup
+
+setup(name='LazyLoader',
+      version='1.0',
+      description='Lazy loader',
+      url='https://github.com/joreeves/LazyLoader',
+      author='Tensorflow',
+      author_email='',
+      license='MIT',
+      packages=['lazyloader'],
+      zip_safe=False)
